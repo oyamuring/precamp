3300e8db3dbab501899e899ca567069d25d73015
oyamuring <skibbb@naver.com>
2024-02-22 20:58:06 +0900

    Dom 사용

---
+++ class/03-javascript/04-document.js
@@ -0,0 +1,11 @@
+function greeting1() {
+
+    document.getElementById("target").innerText = "World"
+
+}
+
+// function greeting2() {
+
+//     document.getElementById("input").value = "World"
+
+// }
